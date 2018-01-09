3259dbe354de94d95bdf5a6e19edcf9cc44d8272
ynakano2100 <ynakano@armg.jp>
2018-01-09 16:23:39 +0900

    test201801091622

---
+++ src/main/java/com/example/demo/BlgSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlgSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlgSpringBootApplication.class, args);
+	}
+}
