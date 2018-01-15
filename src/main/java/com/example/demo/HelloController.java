b066d45a3baee9604df896e52d5a2efa5eef1125
ynakano2100 <ynakano@armg.jp>
2018-01-15 09:40:19 +0900

    201801 commit GitHub

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello World!!";
+		return "Hello World!! Hello GitHub!!";
 	}
 
 }
